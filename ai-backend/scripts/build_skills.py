48b60f50b6318389bab68f405ae1b4b7a4aab015
Coder Kartik <kartikgarg271005@gmail.com>
2026-08-21 12:03:33 +0530

    Added tech_skills.csv

--- ai-backend/scripts/build_skills.py
+++ ai-backend/scripts/build_skills.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Reading required CSV Files
 es = pd.read_csv(os.path.join(BASE_DIR, "../data/raw/essential_skills.csv"))
 ss = pd.read_csv(os.path.join(BASE_DIR, "../data/raw/software_skills.csv"))
+tech = pd.read_csv(os.path.join(BASE_DIR, "../data/processed/tech_skills.csv"))
+
 
 # Extracting required essential_skills.csv data
 unique_el_name = es["Element Name"].unique()
@@ -26,6 +28,7 @@ skill_df = skill_df.drop_duplicates(subset='skill_name')
 # Adding skill_id column
 skill_df['skill_id'] = range(1, len(skill_df) + 1)
 skill_df = skill_df[['skill_id', 'skill_name', 'skill_type']]
+skill_df = pd.concat([skill_df, tech], ignore_index=True)
 
 # Exporting to processed/skills.csv
 skill_df.to_csv(os.path.join(BASE_DIR, "../data/processed/skills.csv"), index=False)
